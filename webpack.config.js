0fcab9d123cf4900802bd993919057a2ee3444a8
ForeverWyq <987043378@qq.com>
2023-10-15 19:38:55 +0800

    update webpack -> 5.x.x 去除无用依赖和代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const projectPath = process.cwd();
 const srcDirPath = path.resolve(__dirname, 'src');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
-  devtool: 'none',
+  devtool: false,
   target: 'node',
   context: path.resolve(projectPath),
   mode: process.env.NODE_ENV,
@@ -54,20 +52,6 @@ module.exports = {
     };
   })(),
   plugins: [
-    // 控制台输出信息
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: [],
-        notes: []
-      },
-      clearConsole: true
-    }),
-    // 忽略某些模块的转换操作
-    new webpack.IgnorePlugin({
-      checkResource(resourcePath) {
-        return false;
-      }
-    }),
     // 清理资源
     new CleanWebpackPlugin({
       verbose: true,
